a69c5b6f0169720515084b6615920ab8f92be995
Michela-DC <micheladelconte@outlook.com>
2022-02-13 21:23:42 +0100

    Jsnack3- corrected isNaN control inside for cicle

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -7,29 +7,24 @@ Esegui questo programma in due versioni, con il for e con il while. */
 // CICLO FOR
 // Creo un array poi creo un ciclo for che deve girare per 5 volte (ovvero per quanti numeri ci devono essere dentro l'array)
 // dentro il for chiedo un numero all'utente, lo pusho dentro l'array, finché non ci saranno 5 numeri dentro l'array (condizione che metto dicendo che l'array deve ciclare per 5 volte)
-
 const numbers = [];
 let somma = 0;
-
 for (i = 0; i <=4; i++){ 
-    const userNumber = parseFloat(prompt('Inserisci un numero'));
+    let userNumber = parseInt(prompt(`Inserisci il ${i+1}° numero`));
     console.log(userNumber);
 
-    if(isNaN(userNumber)){
+    while(isNaN(userNumber)){
         alert('Devi inserire un numero in cifre! Riprova!')
+        userNumber = parseInt(prompt(`Inserisci il ${i+1}° numero`));
+        console.log(userNumber);
     }
-
     numbers.push(userNumber);
-
     somma = somma + numbers[i];
     console.log(`la somma attuale è di ${somma}`);
-
 }
-
 console.log(numbers);
 console.log(`la somma dei numeri dentro l'array è di ${somma}`);
 */
-
 
 // CICLO WHILE
 const numbers = [];
@@ -38,12 +33,12 @@ let somma = 0;
 
 while(numbers.length !== 5){ // la condizione è finché la lunghezza dell'array é diversa da 5
     
-    let userNumber = parseFloat(prompt(`Inserisci il ${i+1}° numero`));
+    let userNumber = parseInt(prompt(`Inserisci il ${i+1}° numero`));
     console.log(userNumber);
 
     while(isNaN(userNumber)){
         alert('Devi inserire un numero in cifre! Riprova!')
-        userNumber = parseFloat(prompt('Inserisci un numero'));
+        userNumber = parseInt(prompt(`Inserisci il ${i+1}° numero`));
         console.log(userNumber);
     }
 
